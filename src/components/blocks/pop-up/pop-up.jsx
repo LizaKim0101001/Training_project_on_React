1222891b18063a53c9276e807e8fdd552f38f236
LizaKim0101001 <89346943+LizaKim0101001@users.noreply.github.com>
2022-01-27 17:01:28 +0500

    close popup window by pressing esc

--- src/components/blocks/pop-up/pop-up.jsx
+++ src/components/blocks/pop-up/pop-up.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Title, { TitleSize } from "../../ui/title/title";
 import { Wrapper, Text, CloseButton, StyledButton } from "./styled";
 import { Ul, Li } from "/src/styled";
 
 function Popup({ fullPrice, adress, isShow, onClose, selectProducts }) {
+  const handlerEsc = (evt) => {
+    if (evt.keyCode === 27) {
+      onClose && onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handlerEsc);
+    return () => {
+      document.removeEventListener("keydown", handlerEsc);
+    };
+  }, []);
   return isShow ? (
     <Wrapper>
       <CloseButton onClick={onClose} />
